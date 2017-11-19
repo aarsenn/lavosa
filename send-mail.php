61e2127c421de6b1850172c9b23dd73ddbfaa3f5
Arsen Terletsky <arsen@Arsens-MacBook-Pro.local>
2017-11-19 18:14:57 +0200

    send mails

--- send-mail.php
+++ send-mail.php
@@ -1,3 +1,13 @@
 <?php
-mail('arsenterletsky@gmail.com', 'Subject', 'Text');
+$subject = 'Покупка клею';
+
+$text = 'Ім\'я: ' . $_POST['firstname'] . '/n Прізвище: ' . $_POST['lastname'] . '/n Телефон: ' . $_POST['phone'] . '/n Пошта: '
+ . $_POST['email'] . '/n Кількість мішків: ' . $_POST['bag_count'] . '/n Спосіб доставки: ' . $_POST['post'] . '/n Адрес доставки та відділення: '
+ . $_POST['address'] . '/n Спосіб оплати: ' . $_POST['payment'];
+
+mail('lovosa20@seznam.cz', $subject, $text);
+mail('satusfeksion@gmail.com', $subject, $text);
+mail('vmblogistic@gmail.com', $subject, $text);
+
+echo ('{ "status": 200 }');
 ?> 
