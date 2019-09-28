d19fbaf1c53413c27cb51da62b3225ebd2abb228
Arsen <arsenterletsky@gmail.com>
2019-09-28 17:50:06 +0300

    edit emails

--- send-mail.php
+++ send-mail.php
@@ -5,9 +5,8 @@ $text = 'Ім\'я: ' . $_POST['firstname'] . '/n Прізвище: ' . $_POST['l
  . $_POST['email'] . '/n Кількість мішків: ' . $_POST['bag_count'] . '/n Спосіб доставки: ' . $_POST['post'] . '/n Адрес доставки та відділення: '
  . $_POST['address'] . '/n Спосіб оплати: ' . $_POST['payment'];
 
-mail('lovosa20@seznam.cz', $subject, $text);
 mail('satusfeksion@gmail.com', $subject, $text);
-mail('vmblogistic@gmail.com', $subject, $text);
+mail('davidson@mail.ru', $subject, $text);
 
 echo ('{ "status": 200 }');
 ?> 
